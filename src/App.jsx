e8fa68dc52fb15a6e256b2621c366338be54f7d3
Azul Montalvo <montalvoazul5@gmail.com>
2025-09-20 00:33:13 -0600

    Ruta de forgotPassword

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route} from "react-router-dom";
 import LoginForm from './components/loginForm.jsx';
 import RegisterForm from './components/registerForm.jsx';
 import ProductosScreen from "./components/ProductosScreen.jsx";
+import ForgotPassword from "./components/forgotPassword.jsx";
 
 
 // export default function App() {
@@ -16,6 +17,7 @@ function App() {
       <Route path="/" element={<LoginForm />} />
       <Route path="/login" element={<LoginForm />} />
       <Route path="/register" element={<RegisterForm />} />
+      <Route path="/forgotpassword" element={<ForgotPassword />} />t
       <Route path="/productos" element={<ProductosScreen />} />
     </Routes>
   );
